93c9ccd0a2d887ce367ba3307ae17356feb21739
ElishamaYomi <73912372+ElishamaYomi@users.noreply.github.com>
2025-07-15 18:21:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit.components.v1 as components
 
 iframe_code = """
 <div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
-  <iframe src="https://www.who.int/data/gho/data/indicators/indicator-details/GHO/number-of-deaths"
+  <iframe src="https://app.powerbi.com/view?r=eyJrIjoiNTQyMjcyZjMtYTAyNy00YzhhLWE5YzYtNDE2NDBhZDlkZDI1IiwidCI6ImYxYWU4ZTFmLWIxOTgtNDNlOC1iZjk0LTFmYmZlNTdkMDI2YyJ9"
           style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;"
           allowfullscreen>
   </iframe>
