ea60a9bfa3c76aa292834d9d18ddf051e59a9d2f
ElishamaYomi <73912372+ElishamaYomi@users.noreply.github.com>
2025-07-15 18:13:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 iframe_code = """
-<div style="position: relative; width: 100%; height: 0; padding-bottom: 60%;">
+<div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
     <iframe src=""https://app.powerbi.com/view?r=eyJrIjoiNTQyMjcyZjMtYTAyNy00YzhhLWE5YzYtNDE2NDBhZDlkZDI1IiwidCI6ImYxYWU4ZTFmLWIxOTgtNDNlOC1iZjk0LTFmYmZlNTdkMDI2YyJ9""
             title="Sickle Cell Climate Risk"
             style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;"
@@ -13,4 +13,4 @@ iframe_code = """
 </div>
 """
 
-components.html(iframe_code, height=600) 
+components.html(iframe_code) 
