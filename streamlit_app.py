d7ac8a99d28f84f9ad522e971d854bbfd41aa4b2
ElishamaYomi <73912372+ElishamaYomi@users.noreply.github.com>
2025-07-15 18:06:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
-import streamlit as st
 
+import streamlit as st
 import streamlit.components.v1 as components
 
 iframe_code = """
-<iframe title="Sickle Cell Climate Risk"
-        width="1024"
-        height="612"
-        src="https://app.powerbi.com/view?r=eyJrIjoiNTQyMjcyZjMtYTAyNy00YzhhLWE5YzYtNDE2NDBhZDlkZDI1IiwidCI6ImYxYWU4ZTFmLWIxOTgtNDNlOC1iZjk0LTFmYmZlNTdkMDI2YyJ9"
-        frameborder="0"
-        allowfullscreen>
-</iframe>
+<div style="position: relative; width: 100%; height: 0; padding-bottom: 60%;">
+    <iframe src=""https://app.powerbi.com/view?r=eyJrIjoiNTQyMjcyZjMtYTAyNy00YzhhLWE5YzYtNDE2NDBhZDlkZDI1IiwidCI6ImYxYWU4ZTFmLWIxOTgtNDNlOC1iZjk0LTFmYmZlNTdkMDI2YyJ9""
+            title="Sickle Cell Climate Risk"
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: none;"
+            frameborder="0"
+
+            allowfullscreen>
+    </iframe>
+</div>
 """
-components.html(iframe_code, height=620, width=1030)
+
+components.html(iframe_code, height=600) 
