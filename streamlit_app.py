63da143f94d3961e3f7b339878adc13972d3c61d
ElishamaYomi <73912372+ElishamaYomi@users.noreply.github.com>
2025-07-15 17:51:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
-"""Streamlit App Sickle Cell Climate Impact Modeling.ipynb
-
 import streamlit as st
 
 import streamlit.components.v1 as components
 
 iframe_code = """
 <iframe title="Sickle Cell Climate Risk"
-        width="1140"
-        height="541.25"
+        width="1024"
+        height="612"
         src="https://app.powerbi.com/view?r=eyJrIjoiNTQyMjcyZjMtYTAyNy00YzhhLWE5YzYtNDE2NDBhZDlkZDI1IiwidCI6ImYxYWU4ZTFmLWIxOTgtNDNlOC1iZjk0LTFmYmZlNTdkMDI2YyJ9"
         frameborder="0"
         allowfullscreen>
 </iframe>
 """
-
-components.html(iframe_code, height=550)
+components.html(iframe_code, height=620)
